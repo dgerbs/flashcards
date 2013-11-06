ddae73eb3a0611ba1f4083e69591188d9de3678d
dgerbs <dojoh@outlook.com>
2013-11-06 18:58:38 -0500

    Add another deck

--- flashcards.rb
+++ flashcards.rb
@@ -1,4 +1,34 @@
-puts "\nWelcome to Flashcards!\n"
+puts "\nWelcome to Language Flashcards!\n"
+
+class Game
+  def initialize
+    @decks = []
+  end
+
+  def << deck
+    @decks << deck
+  end
+
+  def play
+    display_decks
+    puts "Pick a deck: "
+    deck = retrieve_deck
+    deck.play
+  end
+
+  def display_decks
+    @decks.each do |deck|
+      puts deck.name
+    end
+  end
+
+  def retrieve_deck
+    name = gets.chomp
+    @decks.detect do |deck|
+      deck.name == name
+    end
+  end
+end
 
 class Card
   attr_accessor :front, :back
@@ -7,19 +37,41 @@ class Card
     @front = front
     @back = back
   end
+
+  def correct?(guess)
+    guess == @back
+  end
+
+  def play
+    puts "\n#{front} > "
+    guess = gets.chomp
+    if correct? guess
+      puts "Correct!"
+    else
+      puts "Incorrect. The answer is: #{back}."
+    end
+  end
 end
 
 class Deck
-  attr_accessor :cards
+  attr_accessor :name, :cards
   def initialize(name)
     @name = name
     @cards = []
   end
 
-  def <<(card)
+  def << card
     @cards << card
   end
 
+  def play
+    shuffle
+    @cards.each do |card|
+      card.play
+    end
+  end
+
+  
   def shuffle
     @cards.shuffle!
   end
@@ -27,25 +79,23 @@ end
 
 card1 = Card.new("cat", "die Katze")
 card2 = Card.new("dog", "der Hund")
-card3 = Card.new("bird", "die Vögel")
+card3 = Card.new("bird", "die Vogel")
 
-deck = Deck.new("German")
-deck <<(card1)
-deck <<(card2)
-deck <<(card3)
+deck1 = Deck.new("German")
+deck1 << card1
+deck1 << card2
+deck1 << card3
 
-deck.shuffle
+card1 = Card.new("cat", "el gato")
+card2 = Card.new("dog", "el perro")
+card3 = Card.new("bee", "la abeja")
 
-deck.cards.each do |card|
-  front = card.front
-  back = card.back
+deck2 = Deck.new("Spanish")
+deck2 << card1
+deck2 << card2
+deck2 << card3
 
-  print "\n#{front} > "
-  guess = gets.chomp
-
-  if guess == back
-    puts "Correct!"
-  else
-    puts "Incorrect. The answer is #{back}."
-  end
-end
+game = Game.new
+game << deck1
+game << deck2
+game.play
