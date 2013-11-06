8c124b16889b7dcb1d267a19470fcb2f955d483f
dgerbs <dojoh@outlook.com>
2013-11-06 10:00:00 -0500

    Convert to Object Oriented Approach

--- flashcards.rb
+++ flashcards.rb
@@ -1,32 +1,11 @@
 puts "\nWelcome to Flashcards!\n"
 
-card1 = {
-  front: "cat",
-  back: "neko"
-}
-card2 = {
-  front: "dog",
-  back: "inu"
-}
-deck = [card1, card2]
-
-deck.each do |card|
-  front = card[:front]
-  back = card[:back]
-  
-  print "\n#{front} > "
-  guess = gets.chomp
-
-  if guess == back
-    puts "Correct!"
-  else 
-    puts "Incorrect. The answer is #{back}."
+class Card
+  def initialize(front, back)
+    puts "#{front} => #{back}"
   end
 end
 
-# Iterating with an array
-# deck.each do |card|
-#   card_parts = card.split("|") # Returns array
-#   front = card_parts[0]
-#   back = card_parts[1]
-# end
+card1 = Card.new("cat", "neko")
+card2 = Card.new("dog", "inu")
+
