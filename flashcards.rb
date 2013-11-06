340071d925d009ddc5cc16d8783cecf010d1fc89
dgerbs <dojoh@outlook.com>
2013-11-06 10:25:55 -0500

    Simplify

--- flashcards.rb
+++ flashcards.rb
@@ -1,11 +1,13 @@
 puts "\nWelcome to Flashcards!\n"
 
 class Card
+  attr_accessor :front, :back
+  
   def initialize(front, back)
-    puts "#{front} => #{back}"
+    @front = front
+    @back = back
   end
 end
 
 card1 = Card.new("cat", "neko")
 card2 = Card.new("dog", "inu")
-
