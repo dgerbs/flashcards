82ac933baa435586e6ddbb8b51ea5c7633996567
dgerbs <dojoh@outlook.com>
2013-11-06 09:26:23 -0500

    Iterate through deck and evaluate user response

--- flashcards.rb
+++ flashcards.rb
@@ -1,9 +1,20 @@
-puts "\nWelcome to Flashcards!\n\n"
+puts "\nWelcome to Flashcards!\n"
 
-card1 = "cat | Neko"
-card2 = "dog | Inu"
+card1 = "cat|neko"
+card2 = "dog|inu"
 deck = [card1, card2]
 
 deck.each do |card|
-  puts card
+  card_parts = card.split("|") # Returns array
+  front = card_parts[0]
+  back = card_parts[1]
+  
+  print "\n#{front} > "
+  guess = gets.chomp
+
+  if guess == back
+    puts "Correct!"
+  else 
+    puts "Incorrect. The answer is #{back}."
+  end
 end
